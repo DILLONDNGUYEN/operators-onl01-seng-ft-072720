8d84bc132ab48b7f140eedb0456fd0c6b33ba3bf
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-28 11:06:54 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,19 @@
+require 'pry'
 def unsafe?(speed)
-
+#return true if the speed is unsafe and false if it is safe
+  if speed < 40
+    return true
+  elsif speed > 60
+    return true
+  else speed == 50
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
+  <40 || >60 ? true : false
 	
 end
 	
